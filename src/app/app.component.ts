81c9434c80cf913924b758b882cbccfa07b13d5d
remax1993 <remax1993@gmail.com>
2019-04-18 10:59:29 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Plant} from './plant';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular';
+  plant: Plant = new Plant('Лаванда', 'Серебряная листва', 99.9);
+  temp: Plant = new Plant('Лаванда', 'Серебряная листва', 99.9);
+
+  save(): void{
+    this.plant.name = this.temp.name;
+    this.plant.description = this.temp.description;
+    this.plant.price = this.temp.price;
+  }
+
+  cancel(): void{
+    this.temp.name = this.plant.name;
+    this.temp.description = this.plant.description;
+    this.temp.price = this.plant.price;
+  }
 }
+
