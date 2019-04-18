df1630aca92c8ee5ddb792027f528978b62970e9
remax1993 <remax1993@gmail.com>
2019-04-18 17:38:45 +0300

    add *ngIf add style
    вложенные компоненты
    ссылки уровня шаблона

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {PlantService} from "./plant.service";
+import { PlantDetailComponent } from './plant-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlantDetailComponent
   ],
   imports: [
     BrowserModule,
